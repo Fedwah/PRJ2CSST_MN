6e9a034337d360b69dbedee27db324e178d2dc39
Syphax <Syphax@DESKTOP-IUD59RG>
2020-07-06 01:41:37 +0100

    filtrer les detection par unité et region

--- src/beans/session/amdec/detection/DetectionManager.java
+++ src/beans/session/amdec/detection/DetectionManager.java
@@ -1,26 +1,46 @@
 package beans.session.amdec.detection;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import beans.entities.amdec.Detection;
 import beans.session.general.BeanManager;
 
 @Stateless
-public class DetectionManager extends BeanManager<Detection>{
-    @PersistenceContext(unitName = "MN_unit")
+public class DetectionManager extends BeanManager<Detection> {
+    @PersistenceContext( unitName = "MN_unit" )
     EntityManager em;
-    
+
     public DetectionManager() {
-        super(Detection.class);
+        super( Detection.class );
     }
-    
+
     @Override
     public EntityManager getEntityManger() {
         // TODO Auto-generated method stub
         return em;
     }
 
+    public List<Detection> listerUn( String codeun ) {
+         Query q = this.getEntityManger().
+         createQuery( "select b from "+Detection.class.getName()+
+                 " b where b.vehicule.unite.codeUN = :un");
+         q.setParameter( "un", codeun );
+         return q.getResultList();
+    }
 
+    public List<Detection> listerReg( String codeReg ) {
+        Query q =  this.getEntityManger().
+                createQuery( "select b from "+Detection.class.getName()+
+                        " b where b.vehicule.unite.region = :reg" );
+        
+        q.setParameter( "reg", codeReg );
+        return q.getResultList();
+    }
+    
+    
 }
--- src/servlets/amdec/Amdec.java
+++ src/servlets/amdec/Amdec.java
@@ -13,11 +13,13 @@ import beans.entities.amdec.Detection;
 import beans.entities.amdec.enums.Frequence;
 import beans.entities.amdec.enums.Gravite;
 import beans.entities.amdec.enums.NoDetection;
+import beans.entities.utilisateurs.Utilisateur;
 import beans.entities.vehicules.Modele;
 import beans.session.amdec.cause.CauseFactory;
 import beans.session.amdec.cause.CausesManager;
 import beans.session.amdec.defaillance.DefaillanceFactory;
 import beans.session.amdec.defaillance.DefaillanceManager;
+import beans.session.amdec.detection.DetectionFactory;
 import beans.session.amdec.detection.DetectionManager;
 import beans.session.amdec.effet.EffetFactory;
 import beans.session.amdec.effet.EffetManager;
@@ -25,6 +27,7 @@ import beans.session.amdec.instruction.InstructionFactory;
 import beans.session.amdec.instruction.InstructionManager;
 import beans.session.general.page.PageGenerator;
 import beans.session.pieces.PieceManager;
+import beans.session.regions.unites.UniteManager;
 import beans.session.vehicules.marques.modeles.ModeleManager;
 
 /**
@@ -42,19 +45,22 @@ public class Amdec extends HttpServlet {
     private DefaillanceManager  defaiManager;
     @EJB
     private EffetManager        effManager;
-    
+
     @EJB
-    private InstructionManager   instManager;
-    
-    @EJB 
-    private ModeleManager modM;
-    
+    private InstructionManager  instManager;
+
     @EJB
-    private PieceManager pM;
-    
+    private ModeleManager       modM;
+
     @EJB
-    private DetectionManager detM;
-    
+    private PieceManager        pM;
+
+    @EJB
+    private DetectionManager    detM;
+
+    @EJB
+    private UniteManager        uM;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -70,14 +76,25 @@ public class Amdec extends HttpServlet {
     protected void doGet( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
         PageGenerator pg = new PageGenerator( AMDEC, "Analyse AMDEC" );
+        Utilisateur u = pg.getUtilisateur( request );
+        DetectionFactory df = new DetectionFactory();
+
+        if ( u.getCodeun() != null && !u.getCodeun().isEmpty() ) {
+            request.setAttribute( "detections", detM.listerUn( u.getCodeun() ) );
+            
+        }else if ( u.getCodereg() != null && u.getCodereg().isEmpty() ) {
+            request.setAttribute( "detections", detM.listerReg( u.getCodereg() ) );
+        } else {
+            request.setAttribute( "detections", detM.lister() );
+        }
         request.setAttribute( "causes", causeManager.lister() );
         request.setAttribute( "effets", effManager.lister() );
         request.setAttribute( "defai", defaiManager.lister() );
         request.setAttribute( "instructions", instManager.lister() );
         request.setAttribute( "modeles", modM.lister() );
         request.setAttribute( "pieces", pM.lister() );
-        request.setAttribute( "detections", detM.lister() );
-        
+      
+
         request.setAttribute( "gravites", Gravite.values() );
         request.setAttribute( "frequences", Frequence.values() );
         request.setAttribute( "niveaux", NoDetection.values() );
@@ -90,7 +107,7 @@ public class Amdec extends HttpServlet {
      */
     protected void doPost( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
-       
+
         String active = ""; // Pour garder le dernier tab selectionner
 
         // volet cause
@@ -114,24 +131,22 @@ public class Amdec extends HttpServlet {
             active = "defaillance";
             DefaillanceFactory df = new DefaillanceFactory();
             df.createValidateAjouter( request, defaiManager );
-            
+
         }
-        
-        //volet instruction
-        if(request.getParameter( "instruction") !=null) {
-            
+
+        // volet instruction
+        if ( request.getParameter( "instruction" ) != null ) {
+
             active = "instruction";
             InstructionFactory iF = new InstructionFactory();
-            
+
             iF.createValidateAjouter( request, instManager );
-            
+
         }
-        
-        request.setAttribute( "active", active);
+
+        request.setAttribute( "active", active );
         doGet( request, response );
 
-        
-       
     }
 
 }
